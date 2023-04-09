f2047af62db73f7ba6d4e17f2e1aa42e4d0ed801
Grzegorz <grzegorzwowk93@gmail.com>
2023-04-09 12:45:07 +0200

    added timecounter, buttons in html and css

---
+++ js/main.js
@@ -0,0 +1,32 @@
+import { teams } from "./matches.js";
+import { players } from "./players.js";
+
+const daysTime = document.querySelector(".counter__days");
+const hoursTime = document.querySelector(".counter__hours");
+const minutesTime = document.querySelector(".counter__minutes");
+const secondsTime = document.querySelector(".counter__seconds");
+
+let day;
+const checkTime = () => {
+	let currentDate = new Date();
+	let nextDate = new Date("04/16/2023 17:37");
+	let result = nextDate - currentDate;
+
+	let seconds = Math.floor(result / 1000) % 60;
+	let minutes = Math.floor(result / 1000 / 60) % 60;
+	let hours = Math.floor(result / 1000 / 60 / 60) % 24;
+	let days = Math.floor(result / 1000 / 60 / 60 / 24);
+
+	daysTime.textContent = days;
+	hoursTime.textContent = hours;
+	minutesTime.textContent = minutes;
+	secondsTime.textContent = seconds;
+};
+setInterval(checkTime, 1000);
+
+const logPlayers = () => {
+	teams.forEach(match => {
+		console.log(match.goals);
+	});
+};
+logPlayers();
---
+++ js/players.js
@@ -0,0 +1,18 @@
+export const players = [
+	{
+		playerName: "Baranowski Piotr",
+		goals: " 10",
+		assists: "3",
+		yellowCards: "3",
+		redcards: "1",
+		minutes: "392",
+	},
+	{
+		playerName: "Adam",
+		goals: "34",
+		assists: "1",
+		yellowCards: "2",
+		redcards: "8",
+		minutes: "435",
+	},
+];
